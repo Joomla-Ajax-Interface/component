7bf2289a13231c59ca0abb8726643db326ca41f2
Matt Thomas <matt@betweenbrain.com>
2013-08-23 00:31:04 -0400

    Adds plugin error response

--- site/ajax.php
+++ site/ajax.php
@@ -69,7 +69,7 @@ if ($input->get('module'))
 	}
 	else
 	{
-		$error = JText::_sprintf('COM_AJAX_MODULE_NOT_PUBLISHED', 'mod_' . $module);
+		$error = JText::sprintf('COM_AJAX_MODULE_NOT_PUBLISHED', 'mod_' . $module);
 	}
 }
 
@@ -86,7 +86,8 @@ if ($input->get('plugin'))
 	JPluginHelper::importPlugin('ajax');
 	$plugin     = ucfirst($input->get('plugin'));
 	$dispatcher = JDispatcher::getInstance();
-	$results    = $dispatcher->trigger('onAjax' . $plugin);
+	$response   = $dispatcher->trigger('onAjax' . $plugin);
+	$results    = $response ? $response : JText::sprintf('COM_AJAX_NO_PLUGIN_RESPONSE', $plugin);
 }
 
 if (!is_null($error))
